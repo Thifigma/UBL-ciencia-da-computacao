351242330a3aeb21a79b3b0d7ec16f7b73a0f34d
Thigas <thiago.figueiredo@ufpr.br>
2024-01-13 10:31:39 -0300

    Resumo e Opnião Software Livre. Exercicios introdutórios de Python

---
+++ primeiro-semestre/introducao-cc-1/semana1/exerciciosPython/exercicio1.py
@@ -0,0 +1,7 @@
+entradaUsuario = input("Digite o valor: ")
+intervalo = int(entradaUsuario)
+
+if intervalo >= 3 and intervalo < 5:
+    print("Falso")
+else:
+    print("verdadeiro")
---
+++ primeiro-semestre/introducao-cc-1/semana1/exerciciosPython/temperatura.py
@@ -0,0 +1,4 @@
+F = input("Digite o valor de F: ")
+C = ((float(F) - 32) * 5 / 9)
+
+print(C,"°C.")
