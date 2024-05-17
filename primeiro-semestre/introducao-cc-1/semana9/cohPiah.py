931f31ad01f5382f76356c4ad988f42204bd711d
Thigas <thiago.figueiredo@ufpr.br>
2024-05-17 11:32:09 -0300

    Concluido a função calcula_assinatura e suas complementares

--- primeiro-semestre/introducao-cc-1/semana9/cohPiah.py
+++ primeiro-semestre/introducao-cc-1/semana9/cohPiah.py
@@ -1,12 +1,12 @@
 import re
 
-'''A funcao le os valores dos tracos linguisticos do modelo e devolve uma
-assinatura a ser comparada com os textos fornecidos'''
 def le_assinatura():
+    '''A funcao le os valores dos tracos linguisticos do modelo e devolve uma assinatura a ser comparada com os textos fornecidos'''
+    
     print("Bem-vindo ao detector automático de COH-PIAH.")
-    print("Informe a assinatura típica de um aluno infectado:")
+    print("Informe a assinatura típica de um aluno infectado: \n")
 
-    wal = 4.51
+    wal = 4.51 
     ttr = 0.693
     hlr = 0.55
     sal = 70.82
@@ -15,9 +15,8 @@ def le_assinatura():
 
     return [wal, ttr, hlr, sal, sac, pal]
 
-    '''A funcao le todos os textos a serem comparados e devolve uma lista 
-        contendo cada texto como um elemento'''
 def le_textos():
+    '''A funcao le todos os textos a serem comparados e devolve uma lista contendo cada texto como um elemento'''
     i = 1
     textos = []
     texto = input("Digite o texto " + str(i) +" (aperte enter para sair):")
@@ -28,24 +27,23 @@ def le_textos():
 
     return textos
 
-    '''A funcao recebe um texto e devolve uma lista das sentencas dentro do texto'''
 def separa_sentencas(texto):
+    '''A funcao recebe um texto e devolve uma lista das sentencas dentro do texto'''
     sentencas = re.split(r'[.!?]+', texto)
     if sentencas[-1] == '':
         del sentencas[-1]
     return sentencas
 
-    '''A funcao recebe uma sentenca e devolve uma lista das frases dentro da sentenca'''
 def separa_frases(sentenca):
+    '''A funcao recebe uma sentenca e devolve uma lista das frases dentro da sentenca'''
     return re.split(r'[,:;]+', sentenca)
 
-    '''A funcao recebe uma frase e devolve uma lista das palavras dentro da frase'''
 def separa_palavras(frase):
+    '''A funcao recebe uma frase e devolve uma lista das palavras dentro da frase'''
     return frase.split()
 
-    '''Essa funcao recebe uma lista de palavras e devolve o numero de palavras
-        que aparecem uma unica vez'''
 def n_palavras_unicas(lista_palavras):
+    '''Essa funcao recebe uma lista de palavras e devolve o numero de palavras que aparecem uma unica vez'''
     freq = dict()
     unicas = 0
     for palavra in lista_palavras:
@@ -60,8 +58,8 @@ def n_palavras_unicas(lista_palavras):
 
     return unicas
 
-    '''Essa funcao recebe uma lista de palavras e devolve o numero de palavras diferentes utilizadas'''
 def n_palavras_diferentes(lista_palavras):
+    '''Essa funcao recebe uma lista de palavras e devolve o numero de palavras diferentes utilizadas'''
     freq = dict()
     for palavra in lista_palavras:
         p = palavra.lower()
@@ -72,83 +70,88 @@ def n_palavras_diferentes(lista_palavras):
 
     return len(freq)
 
-    '''Essa funcao recebe duas assinaturas de texto e deve devolver 
-        o grau de similaridade nas assinaturas.'''
 def compara_assinatura(as_a, as_b):
-    
-    Sab = 0;
-    i = 0
-    while (i < 6):
-        Sab = Sab + abs(as_a[i] + as_b[i]);
-        i = i + 1;
+    '''IMPLEMENTAR. Essa funcao recebe duas assinaturas de texto e deve devolver o grau de similaridade nas assinaturas.'''
+    pass
 
-    return (Sab / 6) 
-
-def somaCaracter(totalCaracter):
+def constroi_frases(sentencas):
+    ''' Essa funcao retorna uma lista de frases.'''
     
+    lista_frases = []
+
+    for sentenca in sentencas:
+        frase = separa_frases(sentenca)
+        lista_frases.extend(frase)
+
+    return lista_frases
+
+def constroi_palavras(frases):
+    ''' Essa função retorna uma lista de palavras'''
+
+    lista_palavras = []
+
+    for frase in frases:
+        palavra = separa_palavras(frase)
+        lista_palavras.extend(palavra)
+
+    return lista_palavras
+
+def soma_palavras(palavras):
+    ''' Essa função retorna a soma de todas as palavras'''
     soma = 0
-    for i in range(totalCaracter + 1):
-        soma = soma + i
+
+    for palavra in palavras:
+        soma += len(palavra)
 
     return soma
 
-    '''Essa funcao recebe um texto e deve devolver a assinatura do texto.'''
+def soma_caracter(palavras):
+    ''' Essa função retorna a soma de caracter '''
+    soma = 0
+
+    for palavra in palavras:
+        for letra in palavra:
+            soma += 1
+
+    return soma
+
 def calcula_assinatura(texto):
+    '''IMPLEMENTAR. Essa funcao recebe um texto e deve devolver a assinatura do texto.'''
     
-    listaSentencas = separa_sentencas(texto)
-    listaFrases = []
-    listaPalavras = []
-    totalCaracter = 0
-    totalCaracterFrases = 0
-
-    for sentenca in listaSentencas:
-        listaFrases.append(separa_frases(sentenca))
-
-    for i in range(len(listaFrases)):
-        listaPalavras.extend(separa_palavras(*listaFrases[i]))
-
-    for palavra in listaPalavras:
-        totalCaracter = totalCaracter + len(palavra)
+    sentencas = separa_sentencas(texto)
+    frases = constroi_frases(sentencas)
+    palavras = constroi_palavras(frases)
     
-    for frase in listaFrases:
-        totalCaracterFrases = totalCaracterFrases + len(frase)
-
-    wal = (totalCaracter / len(listaPalavras))
-    ttr = (n_palavras_diferentes(listaPalavras) / totalCaracter)
-    hlr = (n_palavras_unicas(listaPalavras) / totalCaracter)
-    sal = (somaCaracter(totalCaracter) / len(listaSentencas))
-    sac = (len(listaFrases) / len(listaSentencas))
-    pal = (somaCaracter(totalCaracterFrases) / len(listaFrases))
-    
+    wal = soma_palavras(palavras) / len(palavras)                            
+    ttr = n_palavras_diferentes(palavras) / len(palavras)
+    hlr = n_palavras_unicas(palavras) / len(palavras)
+    sal = soma_caracter(palavras) / len(sentencas)
+    sac = len(frases) / len(sentencas)
+    pal = soma_caracter(palavras) / len(frases)
+                                       
     return [wal, ttr, hlr, sal, sac, pal]
-    
-    '''Essa funcao recebe uma lista de textos e uma assinatura ass_cp 
-        e deve devolver o numero (1 a n) do texto com maior probabilidade de 
-        ter sido infectado por COH-PIAH.'''
+
 def avalia_textos(textos, ass_cp):
-    ''' Quanto menor o valor mais similar é. 
-        Além disso estou usando a técnica de chutar para depois corrigir. 
-    '''
-    grau_de_similaridade = 200;
-    indice = 0; ''' Indice com o maior grau de similaridade. '''
-    i = 0;
+    '''IMPLEMENTAR. Essa funcao recebe uma lista de textos e uma assinatura ass_cp e deve devolver o numero (1 a n) do texto com maior probabilidade de ter sido infectado por COH-PIAH.'''
+    pass
 
+
+def constroi_assinaturas(textos):
+    ''' Essa função retorna a assinatura de todos os textos.'''
     
-    while (i < len(textos)):
-        if (grau_de_similaridade < compara_assinatura(calcula_assinatura(textos[i]), ass_cp)):
-            grau_de_similaridade = compara_assinatura(calcula_assinatura(textos[i]), ass_cp)
-            indice = i;
+    assinaturas = []
+    
+    for texto in textos:
+        assinaturas.append(calcula_assinatura(texto)) 
 
-        i = i + 1;
-
-    return indice;
-
+    return assinaturas
 
 def main():
-
-    as_Padrao = le_assinatura()
-    textos = le_textos()
     
-    print(avalia_textos(textos, as_Padrao))
+    le_assinatura()
+    textos = le_textos()
+    assinaturas = constroi_assinaturas(textos)
 
-main()
+
+if __name__ == "__main__":
+    main()
